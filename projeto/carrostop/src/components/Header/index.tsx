dc86a15e3ceb5c6acbaaf4c8e682a627d4f887be
hebertaragao <hebert.aragao@outlook.com>
2025-03-06 20:05:45 -0300

    upodate

--- projeto/carrostop/src/components/Header/index.tsx
+++ projeto/carrostop/src/components/Header/index.tsx
@@ -1,7 +1,8 @@
+import './styles.css'
 export default function Header() {
     return (
         <header>
-            <div>
+            <div className="ct-container">
                 <h1>Carros Top</h1>
             </div>
 
